b8c93f98e98c6744453f67448db137c9ff425df2
k.baba <k.baba@cdrive-inc.jp>
2024-05-30 16:50:09 +0900

    create driver2

---
+++ src/main/java/order/Driver2.java
@@ -0,0 +1,36 @@
+package order;
+
+import master.Errcheck;
+
+public class Driver2 {
+
+	public static void main(String args[]){
+
+		numCheck();
+		exitId();
+		notExitId();
+
+
+	}
+	public static void numCheck(){
+		Errcheck err = new Errcheck();
+
+		String msg = err.numCheck("20","1234");
+		System.out.println(msg);
+	}
+
+	public static void exitId(){
+		Errcheck err= new Errcheck();
+
+		String msg = err.exitId("00001");
+		System.out.println("msg="+msg);
+	}
+
+	public static void notExitId(){
+		Errcheck err= new Errcheck();
+
+		String msg = err.notExitId("00010");
+		System.out.println("msg="+msg);
+	}
+
+}
